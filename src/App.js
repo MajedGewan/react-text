4ddd77e1828a9d989908599f642c4ecf92282518
MajedHanGewan <majed.alhanash@gewan.ai>
2024-09-06 21:11:39 +0400

    add styles

--- src/App.js
+++ src/App.js
@@ -12,11 +12,41 @@ function App() {
   };
   return (
     <div className="App">
-      <input placeholder="Your Question" 
+      <textarea placeholder="Your Question"
+      style={{'margin':'10px',  
+              'width':'100%',
+              'max-width': '220px',
+              'height': '45px',
+              'padding': '12px',
+              'border-radius': '12px',
+              'border': '1.5px solid lightgrey',
+              'outline': 'none',
+              'transition': 'all 0.3s cubic-bezier(0.19, 1, 0.22, 1)',
+              'box-shadow': '0px 0px 20px -18px'
+      }} 
       onChange = {(event) => {
         setName(event.target.value);}}
         />
-      <button onClick={fetchData}>Answer</button>
+      <div style={{'display':'block'}}></div>
+      <button
+        style={
+          {
+          'clear': 'both',
+          'width':'100%',
+          'max-width': '220px',
+          'padding': '17px 40px',
+          'border-radius': '50px',
+          'cursor': 'pointer',
+          'border': '0',
+          'background-color': 'green',
+          'box-shadow': 'rgb(0 0 0 / 5%) 0 0 8px',
+          'letter-spacing': '1.5px',
+          'text-transform': 'uppercase',
+          'font-size': '15px',
+          'transition': 'all 0.5s ease',
+          'color':'white'
+        }}
+        onClick={fetchData}>Answer</button>
       <h1>Avatar: {avatarResponse}</h1>
     </div>
   );
